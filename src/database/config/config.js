bc38cb31a58596772282323b350ba4a2d27c8492
RodLopz <rla0006@alu.medac.es>
2024-04-24 15:02:42 +0200

    back to localhost

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,11 @@
-// module.exports = {
-//   "development": {
-//     "username": "root",
-//     "password": null,
-//     "database": "servicios_sql",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql",
-//     "port" : 3306
-//   },
-const mysql = require('mysql');
-
 module.exports = {
   "development": {
-    "connectionLimit": 5,
-    "host": "b3ncolpnxhtep2gfu0qs-mysql.services.clever-cloud.com",
-    "user": "uundp4v4uinyvt91",
-    "password": "AZMojXfHxOnmRCQulfUl",
-    "database": "b3ncolpnxhtep2gfu0qs",
+    "username": "root",
+    "password": null,
+    "database": "servicios_sql",
+    "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": 3306
+    "port" : 3306
   },
   "test": {
     "username": "root",
